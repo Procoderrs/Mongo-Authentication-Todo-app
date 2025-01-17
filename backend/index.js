a3bb6fde4dbb2497232e33dd1820341f60db9f7b
Zeenat <zeenat.zeni0112@gmail.com>
2025-01-17 13:56:55 +0500

    added

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,8 @@ dotenv.config();
 
 const corsOptions = {
   origin: [
-    "http://localhost:5173", // Local development URL
-    "https://todo-authentication.netlify.app" // Netlify site URL
+   // "http://localhost:5173", // Local development URL
+    "https://mongo-authentication-todo-app-tjaq.vercel.app" // Netlify site URL
   ],
   credentials: true, // Allow credentials (cookies, etc.)
 };
