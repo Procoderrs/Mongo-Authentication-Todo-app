3caf55a1c8c118173d8ad6eb35d1d7c5f84975e2
Zeenat <zeenat.zeni0112@gmail.com>
2025-01-22 18:42:03 +0500

    UPDATEPROFILE

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -5,12 +5,20 @@ import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
 import toast from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
 
+
+
+const LOCAL_URL = "http://localhost:5000";
+const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://mongo-todo-authentication.netlify.app";
+const BACKEND_URL = import.meta.env.MODE === "production" ? VITE_BACKEND_URL : LOCAL_URL;
+
+
 const Profile = () => {
   const navigate=useNavigate();
   async function handleLogout() {
     
     try{
-      const respones=await fetch('http://localhost:5000/api/user/logout',{
+      const respones=await fetch(`${BACKEND_URL}/api/user/logout`,{
+        //const res = await fetch(`${BACKEND_URL}/api/user/register`
         method:'POST',
         credentials:'include'
       })
