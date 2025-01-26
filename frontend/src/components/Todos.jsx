80540ebb960148d2e9286377bf8b4333461e0bbc
Zeenat <zeenat.zeni0112@gmail.com>
2025-01-26 17:05:14 +0500

    text

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -441,7 +441,7 @@ const Todos = () => {
                 value={currentTodo}
                 onChange={(e) => setCurrentTodo(e.target.value)}
                 required
-                className="leading-6 py-7 h-12 text-left text-base shadow-md"
+                className="leading-6 py-7 h-12 text-left text-base shadow-md text-black"
               />
 
               <Button className="h-9 rounded-md border border-input px-4 py-7 text-base shadow-md flex items-center hover:bg-primary transition ease-linear group">
