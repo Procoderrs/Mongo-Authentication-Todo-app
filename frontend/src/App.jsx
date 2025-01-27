5cc249ea17120f8f781d8e7eed99253b1cccaccd
Zeenat <zeenat.zeni0112@gmail.com>
2025-01-27 19:02:31 +0500

    updateAppJsFileUseEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Profile from './components/Profile';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 function App() {
   const [user, setUser] = useState(null);
